852ffe2d3ddd313340051d42eb7539fdbf303b89
Juuxel <6596629+Juuxel@users.noreply.github.com>
2021-04-10 14:52:38 +0300

    Bump version to 1.2.0, update dep versions and change notes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     id("org.jetbrains.intellij") version "0.7.2"
-    kotlin("jvm") version "1.4.0" // Corresponds to IDEA 2020.3.2, see kt jars inside the ideaIC dep in IDEA
-    id("org.jmailen.kotlinter") version "3.3.0"
+    kotlin("jvm") version "1.4.32" // Corresponds to IDEA 2021.1, see kt jars inside the ideaIC dep in IDEA
+    id("org.jmailen.kotlinter") version "3.4.0"
 }
 
 group = "me.shedaniel"
-version = "1.1.0"
+version = "1.2.0"
 
 repositories {
     mavenCentral()
