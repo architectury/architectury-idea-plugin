d4fb24eae02a49c4029c7833f4c2697bf5e5e8ed
Juuz <6596629+Juuxel@users.noreply.github.com>
2021-06-20 19:42:03 +0300

    Create the nav handler directly in RelatedMethodLineMarkerProvider

--- src/main/kotlin/me/shedaniel/architectury/idea/gutter/RelatedMethodLineMarkerProvider.kt
+++ src/main/kotlin/me/shedaniel/architectury/idea/gutter/RelatedMethodLineMarkerProvider.kt
@@ -1,19 +1,15 @@
 package me.shedaniel.architectury.idea.gutter
 
 import com.intellij.codeInsight.daemon.DefaultGutterIconNavigationHandler
-import com.intellij.codeInsight.daemon.GutterIconNavigationHandler
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProviderDescriptor
 import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
-import com.intellij.psi.util.PsiTreeUtil
 import me.shedaniel.architectury.idea.util.ArchitecturyBundle
-import java.awt.event.MouseEvent
 import javax.swing.Icon
 
-abstract class RelatedMethodLineMarkerProvider<M : PsiElement> :
-    LineMarkerProviderDescriptor(), GutterIconNavigationHandler<PsiElement> {
+abstract class RelatedMethodLineMarkerProvider<M : PsiElement> : LineMarkerProviderDescriptor() {
     protected abstract val tooltipTranslationKey: String
     protected abstract val navTitleTranslationKey: String
 
@@ -24,34 +20,26 @@ abstract class RelatedMethodLineMarkerProvider<M : PsiElement> :
 
     @Suppress("UNCHECKED_CAST")
     override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<PsiElement>? {
-        fun M.relatedMethods() = converter.toPsiMethod(this)?.relatedMethods ?: emptySet()
+        if (converter.type.isInstance(element)) {
+            val related = converter.toPsiMethod(element as M)?.relatedMethods ?: emptySet()
+            if (related.isEmpty()) return null
 
-        if (converter.type.isInstance(element) && (element as M).relatedMethods().isNotEmpty()) {
             val leaf = converter.getPreferredLeafElement(element)
+            val name = converter.toNameIdentifierOwner(element).name
             return LineMarkerInfo(
                 leaf,
                 leaf.textRange,
                 icon,
                 { ArchitecturyBundle[tooltipTranslationKey] },
-                this,
+                DefaultGutterIconNavigationHandler(
+                    related,
+                    "<html>" + ArchitecturyBundle[navTitleTranslationKey, "<b>$name</b>", related.size]
+                ),
                 GutterIconRenderer.Alignment.LEFT,
                 { ArchitecturyBundle[tooltipTranslationKey] }
             )
         }
 
         return null
-    }
-
-    override fun navigate(e: MouseEvent, elt: PsiElement) {
-        val method = PsiTreeUtil.getParentOfType(elt, converter.type) ?: return
-        val related = converter.toPsiMethod(method)?.relatedMethods ?: return
-
-        if (related.isNotEmpty()) {
-            val name = converter.toNameIdentifierOwner(method).name
-            DefaultGutterIconNavigationHandler<PsiElement>(
-                related,
-                "<html>" + ArchitecturyBundle[navTitleTranslationKey, "<b>$name</b>", related.size]
-            ).navigate(e, elt)
-        }
     }
 }
