9980e2cc75acdf9d673dc45da0e3e01ed485dde1
Nico Mexis <nico.mexis@kabelmail.de>
2021-12-15 15:25:53 +0100

    Update IDEA to 2021.3
    and dependencies also...

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jetbrains.intellij") version "1.0"
-    kotlin("jvm") version "1.4.32" // Corresponds to IDEA 2021.2 EAP, see kt jars inside the ideaIC dep in IDEA
-    id("org.jmailen.kotlinter") version "3.4.4"
+    id("org.jetbrains.intellij") version "1.3.0"
+    kotlin("jvm") version "1.5.10" // Corresponds to IDEA 2021.3, see kt jars inside the ideaIC dep in IDEA
+    id("org.jmailen.kotlinter") version "3.6.0"
 }
 
 group = "dev.architectury"
-version = "1.5.0"
+version = "1.6.0"
 
 repositories {
     mavenCentral()
@@ -15,7 +15,7 @@ repositories {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2021.1")
+    version.set("2021.3")
     plugins.set(listOf("java", "Kotlin"))
 }
 
@@ -25,11 +25,11 @@ tasks {
     }
 
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
     }
 
     patchPluginXml {
         sinceBuild.set("211")
-        untilBuild.set("212.*")
+        untilBuild.set("213.*")
     }
 }
