e3b1e3f35b91f2c1b47b9f9483ce04a4d890f9c0
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-08-04 15:34:12 +0300

    Update Kotlin plugin but still compile against Kotlin 1.6.10
    This should fix some issues with IDE sync and the 1.6.10 plugin
    seemingly not working on Gradle 7.5.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,23 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.jetbrains.intellij") version "1.7.0"
-    // Corresponds to IDEA 2022.1, see KotlinVersion class in ideaIC/3rd-party-rt.jar
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.7.10"
     id("org.jmailen.kotlinter") version "3.11.1"
 }
+
+// Corresponds to IDEA 2022.1, see KotlinVersion class in ideaIC/3rd-party-rt.jar
+val kotlinVersion = "1.6.10"
+val kotlinLanguageVersion = kotlinVersion.substringBeforeLast('.')
 
 group = "dev.architectury"
 version = "1.6.2"
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib", kotlinVersion))
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
@@ -27,6 +34,10 @@ tasks {
 
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "11"
+        kotlinOptions {
+            apiVersion = kotlinLanguageVersion
+            languageVersion = kotlinLanguageVersion
+        }
     }
 
     patchPluginXml {
